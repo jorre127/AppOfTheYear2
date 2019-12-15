31395a11758b5a7b4126a8fce11a04625853ea24
Matisse Faes <matttske@gmail.com>
2019-12-15 16:18:18 +0100

    Piechart added + genreinput changed

--- app/src/main/java/com/example/appoftheyear2/Addgame.java
+++ app/src/main/java/com/example/appoftheyear2/Addgame.java
@@ -8,16 +8,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class Addgame extends AppCompatActivity {
+public class Addgame extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Button addGameButon;
     private EditText nameInput;
     private EditText genreInput;
     private EditText scoreInput;
+    private Spinner genreInnput;
+    private String genreSelection = "";
 
     private SettingsFragment settingsFragment = new SettingsFragment();
 
@@ -35,17 +40,22 @@ public class Addgame extends AppCompatActivity {
 
         addGameButon = findViewById(R.id.addGameButton);
         nameInput = findViewById(R.id.editGameName);
-        genreInput = findViewById(R.id.editGenre);
         scoreInput = findViewById(R.id.ScoreEdit);
+        genreInnput = findViewById(R.id.spinner);
+        String[] genreOptions = new String[]{"Adventure", "Action", "JRPG", "Fighting", "Stealth", "Shooter", "Platformer"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, genreOptions);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genreInnput.setAdapter(adapter);
+        genreInnput.setOnItemSelectedListener(this);
 
 
         addGameButon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!nameInput.getText().toString().equals("") && !genreInput.getText().toString().equals("")) {
+                if (!nameInput.getText().toString().equals("")) {
                     Intent returnIntent = new Intent();
                     returnIntent.putExtra("nameInput", nameInput.getText().toString());
-                    returnIntent.putExtra("genreInput", genreInput.getText().toString());
+                    returnIntent.putExtra("genreInput", genreSelection);
                     returnIntent.putExtra("scoreInput", scoreInput.getText().toString());
                     setResult(Activity.RESULT_OK, returnIntent);
 
@@ -56,5 +66,45 @@ public class Addgame extends AppCompatActivity {
                 }
             }
         });
+
+
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                genreSelection = "Adventure";
+                break;
+            case 1:
+                genreSelection = "Action";
+                break;
+            case 2:
+                genreSelection = "JRPG";
+                break;
+            case 3:
+                genreSelection = "Fighting";
+                break;
+            case 4:
+                genreSelection = "Stealth";
+                break;
+            case 5:
+                genreSelection = "Shooter";
+                break;
+            case 6:
+                genreSelection = "Platformer";
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
--- app/src/main/java/com/example/appoftheyear2/HomeFragment.java
+++ app/src/main/java/com/example/appoftheyear2/HomeFragment.java
@@ -13,11 +13,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
     public static ArrayList<Game> gameList =  new ArrayList<>();
@@ -26,6 +33,16 @@ public class HomeFragment extends Fragment {
     TextView scoreAverage;
     int allscores = 0;
     View mView;
+    PieChart pieChart;
+    ArrayList<String> genreList = new ArrayList<>();
+    float adventureTotal = 0;
+    float actionTotal = 0;
+    float JRPGTotal = 0;
+    float PlatformTotal = 0;
+    float shooterTotal = 0;
+    float stealthTotal = 0;
+    float fightingTotal = 0;
+    float allGenresTotal = 0;
 
     @Override
     public void onAttach(Activity activity) {
@@ -41,7 +58,8 @@ public class HomeFragment extends Fragment {
         mView = view;
         loadData();
         for (Game game: gameList
-             ) {
+        ) {
+            genreList.add(game.getGenre());
             allscores+= game.getScore();
         }
         if(gameList.size()!= 0) {
@@ -50,6 +68,78 @@ public class HomeFragment extends Fragment {
         scoreAverage = mView.findViewById(R.id.scoreAmount);
 
         scoreAverage.setText(String.valueOf(allscores));
+
+        for (String genre: genreList
+             ) {
+            switch (genre.toLowerCase()){
+                case "adventure":
+                    adventureTotal++;
+                break;
+                case "action":
+                    actionTotal++;
+                    break;
+                case "jrpg":
+                    JRPGTotal++;
+                    break;
+                case "platformer":
+                    PlatformTotal++;
+                    break;
+                case "shooter":
+                    shooterTotal++;
+                    break;
+                case "stealth":
+                    stealthTotal++;
+                    break;
+                case "fighting":
+                    fightingTotal++;
+                    break;
+            }
+        }
+
+        allGenresTotal = genreList.size();
+         adventureTotal/=allGenresTotal;
+         actionTotal/=allGenresTotal;
+         JRPGTotal/=allGenresTotal;
+         PlatformTotal/=allGenresTotal;
+         shooterTotal/=allGenresTotal;
+         stealthTotal/=allGenresTotal;
+         fightingTotal/=allGenresTotal;
+
+        pieChart = mView.findViewById(R.id.genrePieChart);
+        pieChart.setUsePercentValues(true);
+        List<PieEntry> genres = new ArrayList<>();
+        if (adventureTotal != 0){
+            genres.add(new PieEntry(adventureTotal, "Adventure"));
+        }
+        if (JRPGTotal != 0) {
+
+            genres.add(new PieEntry(JRPGTotal, "JRPG"));
+        }
+        if (actionTotal != 0) {
+            genres.add(new PieEntry(actionTotal, "Action"));
+        }
+        if (PlatformTotal != 0) {
+            genres.add(new PieEntry(PlatformTotal, "Platformer"));
+        }
+        if (shooterTotal != 0) {
+            genres.add(new PieEntry(shooterTotal, "Shooter"));
+        }
+        if(stealthTotal != 0){
+            genres.add(new PieEntry(stealthTotal, "Stealth"));
+        }
+        if (fightingTotal != 0) {
+            genres.add(new PieEntry(fightingTotal, "Fighting"));
+        }
+
+
+        PieDataSet pieDataSet = new PieDataSet(genres, "Genres");
+        PieData pieData = new PieData(pieDataSet);
+        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        pieChart.setData(pieData);
+        pieChart.animateXY(1400, 1400);
+        Description description = new Description();
+        description.setText("");
+        pieChart.setDescription(description);
         return view;
 
     }
